393a12064b2cb8fe40bdd0a7b6ec69edc861bb60
Sarthak Gondwal <gondwalsarthak192@gmail.com>
2026-05-20 01:04:30 +0530

    Added requirements

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import plotly.express as px
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-client_id = "Your_client_id"
-client_secret = "Your_client_secret"
+client_id = "7b947ce0ea144f75995b8041d644d517"
+client_secret = "9ca65631b5d245c786acf9e18379c4ad"
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyClientCredentials(
